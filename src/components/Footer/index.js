ee5e5d2b07b6f96f7ce1747fe3e992e7034c2d90
Merry31 <julien.rallo@gmail.com>
2016-05-24 22:28:49 +0200

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+/* component styles */
+import { styles } from './styles.scss';
+
+export class Footer extends Component {
+
+  render() {
+    return (
+      <footer className={`${styles}`}>
+        <div className="container">
+          <div className="row">
+            The PolarTeam
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
---
+++ src/components/Header/index.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+/* component styles */
+import { styles } from './styles.scss';
+
+export class Header extends Component {
+  render() {
+    return (
+      <header className={`${styles}`}>
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-5 col-sm-3 col-md-3 col-lg-3 logo">
+              <Link to="/">
+                WineCellar webapp
+              </Link>
+            </div>
+
+            <div className="col-xs-5 col-sm-5 col-md-5 col-lg-5">
+              <nav>
+                <Link to="/home" activeClassName="active">
+                  Home
+                </Link>
+                 <Link to="/cellar" activeClassName="active">
+                  Cellar
+                </Link>
+                <Link to="/list" activeClassName="active">
+                  Events
+                </Link>
+              </nav>
+            </div>           
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
